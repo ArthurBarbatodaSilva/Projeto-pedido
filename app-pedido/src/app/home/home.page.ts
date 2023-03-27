9f89b7c5446f7fd3f646b38de10928d9e7d1f039
unknown <barbatui0311@gmail.com>
2023-03-27 20:15:40 -0300

    alterações

--- app-pedido/src/app/home/home.page.ts
+++ app-pedido/src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -17,20 +16,18 @@ export class HomePage {
 
   items: any[] = [];
   public produtos = [
-    {categoria:'Hamburguer',id : 1, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'Burgão1' , valor:12.50, imagem:['Hamburger.jpg'], status: false},
-    {categoria:'Hamburguer',id : 2, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'Burgão2' , valor:10.00, imagem:['Hamburger.jpg'], status: false},
-    {categoria:'Salgados',id : 3, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'salgado1' , valor:8.00, imagem:['Salgado.jpg'], status: false},
-    {categoria:'Salgados',id : 4, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'salgado2' , valor:7.00, imagem:['Salgado.jpg'], status: false},
-    {categoria:'Bebidas',id : 5, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'bebida1' , valor:3.50, imagem:['Bebida.jpg'], status: false},
-    {categoria:'Bebidas',id : 6, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'bebida2' , valor:3.50, imagem:['Bebida.jpg'], status: false},
+    {categoria:'Hamburguer',id : 1, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'Burgão1' , valor:12.50, imagem:['Hamburger.jpg']},
+    {categoria:'Hamburguer',id : 2, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'Burgão2' , valor:10.00, imagem:['Hamburger.jpg']},
+    {categoria:'Salgados',id : 3, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'salgado1' , valor:8.00, imagem:['Salgado.jpg']},
+    {categoria:'Salgados',id : 4, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'salgado2' , valor:7.00, imagem:['Salgado.jpg']},
+    {categoria:'Bebidas',id : 5, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'bebida1' , valor:3.50, imagem:['Bebida.jpg']},
+    {categoria:'Bebidas',id : 6, detalhes :'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',nome:'bebida2' , valor:3.50, imagem:['Bebida.jpg']},
   ]
 
   public pathImgs = '../../assets/';
   
 
-  constructor(private navController: NavController) {
-    this.navController
-
+  constructor() {
     this.items = this.produtos;
   }
 
